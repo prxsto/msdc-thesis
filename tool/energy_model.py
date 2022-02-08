e9dee9c32410c39a8fcb3855c2923a582771f2ee
preston pape <papepreston@gmail.com>
2022-02-07 19:46:10 -0800

    nothing

--- tool/energy_model.py
+++ tool/energy_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
 import numpy as np
@@ -26,21 +26,29 @@ X, y = data.iloc[:,:-1], data.iloc[:,-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
 model = xgb.XGBRegressor()
-parameters = {
+params = {
     "max_depth": np.arange(3, 50, 1), 
     "gamma": np.arange(0, 20, 1), 
     "lambda": np.arange(0, 1, .1)
     }
+# params['eval_metric'] = 'mae'
 
-randomized_search = RandomizedSearchCV(model, parameters, n_iter=20, 
-                                       scoring="neg_mean_squared_error", cv=3)
+randomized_search = RandomizedSearchCV(model, params, n_iter=20, 
+                                       scoring="neg_mean_absolute_error", cv=3)
+                                       #scoring='neg_mean_squared_error'
 randomized_search.fit(X_train, y_train)
 
 # examine error of optimized model
 xgboost_reg = randomized_search.best_estimator_
 preds = xgboost_reg.predict(X_test)
-rmse = np.sqrt(mean_squared_error(y_test, preds))
-print("RMSE: %f" % (rmse))
+
+# rmse loss
+# rmse = np.sqrt(mean_squared_error(y_test, preds))
+# print("RMSE: %f" % (rmse))
+
+# mae loss
+mae = mean_absolute_error(y_test, preds)
+print("RMSE: %f" % (mae))
 
 pickle_out = open('xgboost_reg.pkl','wb')
 pickle.dump(xgboost_reg, pickle_out)
--- tool/test.py
+++ tool/test.py
@@ -1,9 +1,3 @@
-import os
-import pandas as pd
+import numpy as np
 
-# print(os.path.dirname(__file__))
-dir = os.path.join(os.path.dirname(__file__), "results")
-filename = ('1k_results.csv')
-filepath = os.path.join(dir, filename)
-
-# dadus = pd.read_csv(filepath)
+print(np.arange(0, 1, .1))
