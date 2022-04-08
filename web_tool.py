67917c46657516604621a2d29b53b1dc84d245c9
preston pape <papepreston@gmail.com>
2022-04-08 01:10:36 -0700

    pls

--- web_tool.py
+++ web_tool.py
@@ -182,7 +182,7 @@ def plot_scatter(x, y, color, x_axis_data, y_axis_data):
                         marker_color=color,
                         text=color,
                         mode='markers',
-                        hovertemplate=f'{x_label}: {x}, {y_label}: {y}',
+                        hovertemplate=f'<br>{x_label}:%{x}<br>{y_label}:%{y}',
                         marker= {
                             'size': 12,
                             'colorscale': 'Viridis',
